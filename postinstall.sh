14748405b8bc2eda381b215b93d63d49e4dd0fea
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-09 09:29:01 +0200

    .

--- postinstall.sh
+++ postinstall.sh
@@ -3,5 +3,8 @@
 export MODE="${MODE:-Debug}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 
+echo -- detecting platform: 
 $CMAKI_INSTALL/cmaki_identifier
+# TODO: check return code (check is valid platform)
+echo -- ok
 
