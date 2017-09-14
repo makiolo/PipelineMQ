c30c31326822fca54ffb36a6381b2ec12d8dda56
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-14 21:42:39 +0200

    .

--- postinstall.sh
+++ postinstall.sh
@@ -5,8 +5,7 @@ export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
 
 echo -- detecting platform: 
-ls $CMAKI_INSTALL
-echo $CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
-$CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
+$CMAKI_EMULATOR -h
+$CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier.exe
 echo -- ok
 
