2684d32af342082804157416c1d9d8c102fb8ffc
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-14 21:44:26 +0200

    .

--- postinstall.sh
+++ postinstall.sh
@@ -4,6 +4,8 @@ export MODE="${MODE:-Debug}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
 
+sudo apt-get install -y wine
+
 echo -- detecting platform: 
 $CMAKI_EMULATOR -h
 $CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier.exe
