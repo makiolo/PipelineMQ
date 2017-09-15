2e9081019b58047fe4780bac6d5f039088419ccd
Ricardo <makiolo@gmail.com>
2017-09-15 18:01:55 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,3 +1,8 @@
 @echo off
-echo hola mundo
-
+if exist "boostorg_predef" (
+	rmdir /s /q boostorg_predef
+)
+git clone -q https://github.com/boostorg/predef.git boostorg_predef
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd -o setup.cmd
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd -o compile.cmd
+setup.cmd && compile.cmd
