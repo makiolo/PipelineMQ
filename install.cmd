3c668af8931b03bd0dd0aa56f74bc87d2d616be6
Ricardo <makiolo@gmail.com>
2017-09-15 18:04:13 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -3,6 +3,5 @@ if exist "boostorg_predef" (
 	rmdir /s /q boostorg_predef
 )
 git clone -q https://github.com/boostorg/predef.git boostorg_predef
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd -o setup.cmd
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd -o compile.cmd
-setup.cmd && compile.cmd
+powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
