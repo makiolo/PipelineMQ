5f70b2fd7389f3cb23e631e4579c365d07f8632a
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-16 20:41:07 +0200

    .

--- cmaki_identifier.sh
+++ cmaki_identifier.sh
@@ -15,7 +15,7 @@ elif [[ "$ANDROID_NDK_REVISION" = "13b" ]]; then
 	unset LD_LIBRARY_PATH
 	qemu-arm -L /usr/arm-linux-gnueabi $CMAKI_INSTALL/cmaki_identifier
 elif [[ "$EMSDK" = "/emsdk_portable" ]]; then
-	nodejs $CMAKI_INSTALL/cmaki_identifier.js
+	(cd $DIRSCRIPT && nodejs $CMAKI_INSTALL/cmaki_identifier.js)
 else
 	$CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
 fi
