7bd03c84bc04ecad9f550d3e79755175badc88c6
Ricardo <makiolo@gmail.com>
2017-09-19 00:19:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo  ------------------- begin install.sh @ cmaki_identifier
 if [ -d "boostorg_predef" ]; then
 	rm -Rf boostorg_predef
 fi
 git clone -q https://github.com/boostorg/predef.git boostorg_predef
 curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.sh | bash
 curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.sh | CMAKI_TARGET=cmaki_identifier bash
-
+echo  ------------------- end install.sh @ cmaki_identifier
