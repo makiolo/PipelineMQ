e1f5f5d6e42c33205c2f84ec72492a3177ea2cec
Ricardo <makiolo@gmail.com>
2017-09-18 10:36:14 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -2,6 +2,12 @@
 if exist "boostorg_predef" (
 	rmdir /s /q boostorg_predef
 )
+
 git clone -q https://github.com/boostorg/predef.git boostorg_predef
-powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
-powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+
+:: powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+:: powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd > setup.cmd
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd > compile.md
+call setup.cmd && call compile.cmd
