5e0bbe9270796898af09313533e78e8b05c580bc
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-19 00:27:10 +0200

    .

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -d "boostorg_predef" ]; then
-	rm -Rf boostorg_predef
-fi
-git clone -q https://github.com/boostorg/predef.git boostorg_predef
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.sh | bash
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.sh | CMAKI_TARGET=cmaki_identifier bash
-
---
+++ preinstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ -d "boostorg_predef" ]; then
+	rm -Rf boostorg_predef
+fi
+git clone -q https://github.com/boostorg/predef.git boostorg_predef
+
