8071c8df03bb97d6e11a8aff0430e9eb45110666
Ricardo <makiolo@gmail.com>
2017-09-18 10:08:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/tests.sh | bash
+curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/test.sh | bash
