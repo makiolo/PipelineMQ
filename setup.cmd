755ef7f0f2bb1755d529375537c36ed53fe75b35
Ricardo <makiolo@gmail.com>
2017-09-19 10:21:01 +0200

    Update and rename preinstall.cmd to setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -2,22 +2,12 @@
 if exist "boostorg_predef" (
 	rmdir /s /q boostorg_predef
 )
-
 git clone -q https://github.com/boostorg/predef.git boostorg_predef
-
-set CMAKI_TARGET=cmaki_identifier
 
 if exist "__setup.cmd" (
 	del __setup.cmd
 )
-if exist "__compile.cmd" (
-	del __compile.cmd
-)
+
 curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd > __setup.cmd
 call __setup.cmd
 del __setup.cmd
-
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/compile.cmd > __compile.cmd
-call __compile.cmd
-del __compile.cmd
-
