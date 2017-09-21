cedcf6e20eb2b239af31e183f85c3e10c0e423fa
Ricardo <makiolo@gmail.com>
2017-09-21 16:39:47 +0200

    Update cmaki_emulator.sh

--- cmaki_emulator.sh
+++ cmaki_emulator.sh
@@ -7,6 +7,7 @@
 
 export _DIRNAMEPROGRAM=$(dirname "$1")
 export DIRPROGRAM="$( cd "$_DIRNAMEPROGRAM" && pwd )"
+export BASENAMEPROGRAM=$(basename "$1")
 export CMAKI_PWD="${CMAKI_PWD:-$(pwd)}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
@@ -15,18 +16,19 @@ echo "DIRPROGRAM = $DIRPROGRAM"
 echo "CMAKI_PWD = $CMAKI_PWD"
 echo "CMAKI_INSTALL = $CMAKI_INSTALL"
 echo "CMAKI_EMULATOR = $CMAKI_EMULATOR"
+echo "BASENAMEPROGRAM = $BASENAMEPROGRAM"
 
-shift
+cd $DIRPROGRAM
 if [[ "$WINEARCH" = "win32" ]]; then
-	wine $PROGRAM.exe "$@"
+	wine $BASENAMEPROGRAM.exe "${@:2}"
 elif [[ "$WINEARCH" = "win64" ]]; then
-	wine $PROGRAM.exe "$@"
+	wine $BASENAMEPROGRAM.exe "${@:2}"
 elif [[ "$ANDROID_NDK_REVISION" = "13b" ]]; then
 	unset LD_LIBRARY_PATH
-	qemu-arm -L /usr/arm-linux-gnueabi $PROGRAM "$@"
+	qemu-arm -L /usr/arm-linux-gnueabi $BASENAMEPROGRAM "${@:2}"
 elif [[ "$EMSDK" = "/emsdk_portable" ]]; then
-	nodejs $PROGRAM.js "$@"
+	nodejs $BASENAMEPROGRAM.js "${@:2}"
 else
-	echo $CMAKI_EMULATOR $PROGRAM "$@"
-	$CMAKI_EMULATOR $PROGRAM "$@"
+	echo $CMAKI_EMULATOR $BASENAMEPROGRAM "${@:2}"
+	$CMAKI_EMULATOR $BASENAMEPROGRAM "${@:2}"
 fi
