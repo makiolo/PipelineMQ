45e0d5c1e124bf3f22e768de4b5dbe5b76599255
Ricardo <makiolo@gmail.com>
2017-09-26 00:02:48 +0200

    Update cmaki_emulator.sh

--- cmaki_emulator.sh
+++ cmaki_emulator.sh
@@ -27,8 +27,7 @@ elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/android-arm" ]]; then
 	unset LD_LIBRARY_PATH
 	qemu-arm -L /usr/arm-linux-gnueabi ./$BASENAMEPROGRAM "${@:2}"
 elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/linux-armv6" ]]; then
-	unset LD_LIBRARY_PATH
-	qemu-arm -L /usr/arm-linux-gnueabihf ./$BASENAMEPROGRAM "${@:2}"
+	qemu-arm ./$BASENAMEPROGRAM "${@:2}"
 elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/browser-asmjs" ]]; then
 	nodejs ./$BASENAMEPROGRAM "${@:2}"
 else
