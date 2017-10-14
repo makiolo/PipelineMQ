3622f6cd53062839a91e67e0fdb4aca56d57410e
Ricardo <makiolo@gmail.com>
2017-10-14 15:48:27 +0200

    Update cmaki_emulator.sh

--- cmaki_emulator.sh
+++ cmaki_emulator.sh
@@ -11,6 +11,7 @@ export BASENAMEPROGRAM=$(basename "$1")
 export CMAKI_PWD="${CMAKI_PWD:-$(pwd)}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
+
 # 
 # echo "DIRPROGRAM = $DIRPROGRAM"
 # echo "CMAKI_PWD = $CMAKI_PWD"
@@ -18,19 +19,23 @@ export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
 # echo "CMAKI_EMULATOR = $CMAKI_EMULATOR"
 # echo "BASENAMEPROGRAM = $BASENAMEPROGRAM"
 # 
+
+# TODO: dont use identification based in docker image
+# avoid acoplation with docker here
+
 cd $DIRPROGRAM
-if [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/windows-x86" ]]; then
+if [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/windows-x86" ]]; then
 	wine ./$BASENAMEPROGRAM "${@:2}"
-elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/windows-x64" ]]; then
+elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/windows-x64" ]]; then
 	wine ./$BASENAMEPROGRAM "${@:2}"
-elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/android-arm" ]]; then
+elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/android-arm" ]]; then
 	unset LD_LIBRARY_PATH
 	qemu-arm -L /usr/arm-linux-gnueabi ./$BASENAMEPROGRAM "${@:2}"
-elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/linux-armv6" ]]; then
+elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/linux-armv6" ]]; then
 	qemu-arm ./$BASENAMEPROGRAM "${@:2}"
-elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/linux-armv7" ]]; then
+elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/linux-armv7" ]]; then
 	qemu-arm ./$BASENAMEPROGRAM "${@:2}"
-elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "dockcross/browser-asmjs" ]]; then
+elif [[ "$DEFAULT_DOCKCROSS_IMAGE" = "makiolo/browser-asmjs" ]]; then
 	nodejs ./$BASENAMEPROGRAM "${@:2}"
 else
 	$CMAKI_EMULATOR ./$BASENAMEPROGRAM "${@:2}"
