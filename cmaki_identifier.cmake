e304a0c25df3df90247e8753d4e6ea41f6c23922
Ricardo Marmolejo García <makiolo@gmail.com>
2017-10-14 20:38:53 +0200

    try fix windows x86

--- cmaki_identifier.cmake
+++ cmaki_identifier.cmake
@@ -1,6 +1,10 @@
 set(PLATFORM "")
 set(dirscript ${CMAKE_CURRENT_LIST_DIR})
-find_program(path cmaki_identifier ${dirscript})
+IF(WIN32)
+	find_program(path cmaki_identifier.exe ${dirscript})
+else()
+	find_program(path cmaki_identifier.sh ${dirscript})
+endif()
 execute_process(COMMAND ${path}
 			WORKING_DIRECTORY ${dirscript}
 			OUTPUT_VARIABLE PLATFORM
--- cmaki_identifier.sh
+++ cmaki_identifier.sh
@@ -7,4 +7,9 @@ export CMAKI_PWD="${CMAKI_PWD:-$DIRSCRIPT}/.."
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
 
-$DIRSCRIPT/cmaki_emulator.sh $CMAKI_INSTALL/cmaki_identifier
+if [ -f "cmaki_identifier.exe" ]; then
+	$DIRSCRIPT/cmaki_emulator.sh $CMAKI_INSTALL/cmaki_identifier.exe
+else
+	$DIRSCRIPT/cmaki_emulator.sh $CMAKI_INSTALL/cmaki_identifier
+fi
+
